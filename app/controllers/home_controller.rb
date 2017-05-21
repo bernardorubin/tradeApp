82b8860cb5975ce2c9c1205253823cca39f4b986
Jose Zuniga <jramon.zu@gmail.com>
2017-05-21 16:25:54 -0500

    Tables for etherum and ripple

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,9 +5,13 @@ class HomeController < ApplicationController
 
     # hardcoded params until conn is working!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     @response_book_btc = Faraday.get 'https://api.bitso.com/v3/order_book?book=btc_mxn&aggregate=true'
-    @response_trades_btc = Faraday.get 'https://api.bitso.com/v3/trades?book=btc_mxn&sort=desc&limit=100'
+    @response_trades_btc = Faraday.get 'https://api.bitso.com/v3/trades?book=btc_mxn&sort=desc&limit=10'
 
+    @response_book_eth = Faraday.get 'https://api.bitso.com/v3/order_book?book=eth_mxn&aggregate=true'
+    @response_trades_eth = Faraday.get 'https://api.bitso.com/v3/trades?book=eth_mxn&sort=desc&limit=10'
 
+    @response_book_xrp = Faraday.get 'https://api.bitso.com/v3/order_book?book=xrp_mxn&aggregate=true'
+    @response_trades_xrp = Faraday.get 'https://api.bitso.com/v3/trades?book=xrp_mxn&sort=desc&limit=10'
 
 
     # render json:@response.body
@@ -49,42 +53,118 @@ class HomeController < ApplicationController
      end
 
 
-
      @book_btc = JSON.parse(@response_book_btc.body)
-     @bids_prices = []
-     @asks_prices = []
+     @bids_prices_btc = []
+     @asks_prices_btc = []
     #  Uncomment if needed
     #  @bids_amounts = []
     #  @asks_amounts = []
-     @payload_book = @book_btc["payload"]
-     @updated = @payload_book["updated_at"]
-     @bids = @payload_book["bids"]
-     @bids.each do |y|
-       @bids_prices.push(y["price"])
+     @payload_book_btc = @book_btc["payload"]
+     @updated_btc = @payload_book_btc["updated_at"]
+     @bids_btc = @payload_book_btc["bids"]
+     @bids_btc.each do |y|
+       @bids_prices_btc.push(y["price"])
      end
-     @asks = @payload_book["asks"]
-     @asks.each do |y|
-       @asks_prices.push(y["price"])
+     @asks_btc = @payload_book_btc["asks"]
+     @asks_btc.each do |y|
+       @asks_prices_btc.push(y["price"])
      end
 
 
      @trades_btc = JSON.parse(@response_trades_btc.body)
-     @fz = []
-     @date = []
-     @amount = []
-     @side = []
-     @price = []
-     @maker_side = []
-     @trade_id = []
-     @payload_trades = @trades_btc["payload"]
-     @payload_trades.each do |y|
-       @fz << y["book"]
-       @date << y["created_at"]
-       @amount << y["amount"]
-       @maker_side << y["maker_side"]
-       @price << y["price"]
-       @trade_id << y["tid"]
+     @fx_btc = []
+     @date_btc = []
+     @amount_btc = []
+     @side_btc = []
+     @price_btc = []
+     @maker_side_btc = []
+     @trade_id_btc = []
+     @payload_trades_btc = @trades_btc["payload"]
+     @payload_trades_btc.each do |y|
+       @fx_btc << y["book"]
+       @date_btc << y["created_at"]
+       @amount_btc << y["amount"]
+       @maker_side_btc << y["maker_side"]
+       @price_btc << y["price"]
+       @trade_id_btc << y["tid"]
+    end
+
+
+
+
+     @book_eth = JSON.parse(@response_book_eth.body)
+     @bids_prices_eth = []
+     @asks_prices_eth = []
+    #  Uncomment if needed
+    #  @bids_amounts = []
+    #  @asks_amounts = []
+     @payload_book_eth = @book_eth["payload"]
+     @updated_eth = @payload_book_eth["updated_at"]
+     @bids_eth = @payload_book_eth["bids"]
+     @bids_eth.each do |y|
+       @bids_prices_eth.push(y["price"])
      end
+     @asks_eth = @payload_book_eth["asks"]
+     @asks_eth.each do |y|
+       @asks_prices_eth.push(y["price"])
+     end
+
+
+     @trades_eth = JSON.parse(@response_trades_eth.body)
+     @fx_eth = []
+     @date_eth = []
+     @amount_eth = []
+     @side_eth = []
+     @price_eth = []
+     @maker_side_eth = []
+     @trade_id_eth = []
+     @payload_trades_eth = @trades_eth["payload"]
+     @payload_trades_eth.each do |y|
+       @fx_eth << y["book"]
+       @date_eth << y["created_at"]
+       @amount_eth << y["amount"]
+       @maker_side_eth << y["maker_side"]
+       @price_eth << y["price"]
+       @trade_id_eth << y["tid"]
+    end
+
+
+
+     @book_xrp = JSON.parse(@response_book_xrp.body)
+     @bids_prices_xrp = []
+     @asks_prices_xrp = []
+    #  Uncomment if needed
+    #  @bids_amounts = []
+    #  @asks_amounts = []
+     @payload_book_xrp = @book_xrp["payload"]
+     @updated_xrp = @payload_book_xrp["updated_at"]
+     @bids_xrp = @payload_book_xrp["bids"]
+     @bids_xrp.each do |y|
+       @bids_prices_xrp.push(y["price"])
+     end
+     @asks_xrp = @payload_book_xrp["asks"]
+     @asks_xrp.each do |y|
+       @asks_prices_xrp.push(y["price"])
+     end
+
+
+     @trades_xrp = JSON.parse(@response_trades_xrp.body)
+     @fx_xrp = []
+     @date_xrp = []
+     @amount_xrp = []
+     @side_xrp = []
+     @price_xrp = []
+     @maker_side_xrp = []
+     @trade_id_xrp = []
+     @payload_trades_xrp = @trades_xrp["payload"]
+     @payload_trades_xrp.each do |y|
+       @fx_xrp << y["book"]
+       @date_xrp << y["created_at"]
+       @amount_xrp << y["amount"]
+       @maker_side_xrp << y["maker_side"]
+       @price_xrp << y["price"]
+       @trade_id_xrp << y["tid"]
+    end
 
 
   end
