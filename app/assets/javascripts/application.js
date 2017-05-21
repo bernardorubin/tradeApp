5dc74fe34a1a7043d944f4f04c334c3ea45f70f7
bernulander <bernulander@gmail.com>
2017-05-21 00:04:27 -0500

    added charts and bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,5 +10,17 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+
+
 //= require rails-ujs
 //= require_tree .
+
+
+
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
+
+//= require Chart.bundle
+//= require chartkick
